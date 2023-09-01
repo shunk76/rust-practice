6908818eb8268fd5ec69047e7a70a4989d823b17
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-01 13:54:19 +0900

    add src/ch/11_caesar_enc.rs & main.rs

---
+++ src/ch1/11_caesar_enc.rs
@@ -0,0 +1,26 @@
+fn encrypt(text: &str, shift: i16) -> String {
+    // 'A' と 'Z' の文字コードを i16 型で得る
+    let code_a = 'A' as i16;
+    let code_z = 'Z' as i16;
+
+    let mut result = String::new();
+
+    for ch in text.chars() {
+        let mut code = ch as i16;
+
+        if code_a <= code && code <= code_z {
+            code = (code - code_a + shift + 26) % 26 + code_a;
+        }
+
+        result.push((code as u8) as char);
+    }
+
+    return result;
+}
+
+fn main() {
+    let enc = encrypt("I LOVE YOU", 3);
+    let dec = encrypt(&enc, -3);
+
+    println!("{} => {}", dec, enc);
+}
