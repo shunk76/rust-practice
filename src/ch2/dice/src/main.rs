8ee7736f62cedf47ac6be77c93bad26d914305ac
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-05 10:00:09 +0900

    add src/ch2/dice

---
+++ src/ch2/dice/src/main.rs
@@ -0,0 +1,21 @@
+// P.125
+
+use rand::{thread_rng, Rng};
+
+fn main() {
+    let mut rng = thread_rng();
+    let mut counts = [0; 6];
+
+    let end = 10000;
+    let f_end = end as f64;
+
+    for _ in 0..end {
+        let dice = rng.gen_range(1..=6);
+        counts[dice - 1] += 1;
+    }
+
+    println!("出目の回数: {:?}", counts);
+    counts
+        .iter()
+        .for_each(|&x| println!("{}", x as f64 / f_end));
+}
