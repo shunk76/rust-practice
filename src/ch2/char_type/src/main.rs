c0e70a3a2485df463144d864baec4087c97c65c9
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-09 08:39:05 +0900

    add src/ch2/char_type

---
+++ src/ch2/char_type/src/main.rs
@@ -0,0 +1,23 @@
+// P.155
+
+fn main() {
+    let a = 'a';
+    let b = b'a';
+    let c = '\x61';
+    println!("{}, {:2x}, {}", a, b, c);
+
+    let d = '愛';
+    let e = '愛' as u32;
+    let f = '\u{611b}';
+    println!("{}, {:4x}, {}", d, e, f);
+
+    let v1 = 0xFF;
+    let v2 = 0o655;
+    let v3 = 0b1101_0101;
+    println!("{}, {}, {}", v1, v2, v3);
+
+    let f1 = 10.5;
+    let f2 = 10.5f32;
+    let f3 = 10.5e+8;
+    println!("{}, {}, {}", f1, f2, f3);
+}
