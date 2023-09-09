6b112572168c233de023981060668e0f7faced4e
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-09 16:39:35 +0900

    add src/ch2/read_file

---
+++ src/ch2/read_file/src/main.rs
@@ -0,0 +1,18 @@
+// P.169
+// cargo run src/test.txt で実行
+
+use std::env;
+use std::fs;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        println!("入力ファイルを指定してください。");
+        return;
+    }
+
+    let filename = &args[1];
+    let text = fs::read_to_string(filename).unwrap();
+    println!("{}", text);
+}
