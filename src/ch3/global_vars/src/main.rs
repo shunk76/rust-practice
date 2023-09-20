093154f42be76b2c0c3e1d25e05d1de0da65c6e3
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-20 15:01:26 +0900

    add src/ch3/global_vars

---
+++ src/ch3/global_vars/src/main.rs
@@ -0,0 +1,13 @@
+// P.278
+
+// グローバル変数を定義
+static mut TAX: f32 = 0.1;
+
+fn main() {
+    unsafe {
+        println!("Price: {}", TAX * 300.0);
+
+        TAX = 0.08;
+        println!("Price: {}", TAX * 300.0);
+    }
+}
