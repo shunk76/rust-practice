fa4c02c45f4cd90e3a8ccbef64af5ee88942f1c7
Shunsuke Kusakabe <shunsuke.kusakabe@gmail.com>
2023-09-22 20:04:49 +0900

    add src/ch4/struct_copy_fix

---
+++ src/ch4/struct_copy_fix/src/main.rs
@@ -0,0 +1,26 @@
+// P.302
+
+struct Person {
+    name: String,
+    age: i32,
+}
+
+impl Person {
+    fn new(name: &str, age: i32) -> Self {
+        Self {
+            name: name.to_string(),
+            age,
+        }
+    }
+}
+
+fn main() {
+    let taro = Person::new("Taro", 18);
+    let jiro = Person {
+        name: String::from("Jiro"),
+        ..taro // 更新記法
+    };
+
+    println!("{}, {}", taro.name, taro.age);
+    println!("{}, {}", jiro.name, jiro.age);
+}
